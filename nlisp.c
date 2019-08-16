a287cf655c65c24a2a1b79a192394db095decb76
athxp <athxp@tutanota.com>
2019-08-17 05:06:53 +0700

    Eval

--- nlisp.c
+++ nlisp.c
@@ -1,8 +1,5 @@
-//#include <stdio.h>
-//#include <stdlib.h>
 #include "mpc.h"
 #ifdef _WIN32
-//#include <string.h>
 static char buffer[2048];
 
 char* readline(char* prompt)
@@ -20,6 +17,29 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
+
+long eval_op(long x, char* op, long y)
+{
+	if (strcmp(op, "+") == 0) return x + y;
+	if (strcmp(op, "-") == 0) return x - y;
+	if (strcmp(op, "*") == 0) return x * y;
+	if (strcmp(op, "/") == 0) return x / y;
+	return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+	if (strstr(t->tag, "number"))
+		return atoi(t->contents);
+	char* op = t->children[1]->contents;
+	long x = eval(t->children[2]);
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+	return x;
+}
 
 int main(int argc, char** argv)
 {
@@ -50,11 +70,10 @@ int main(int argc, char** argv)
 		/* Attempt to parse the user input */
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Nlisp, &r)) {
-			/* On success print and delete the AST */
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
-			/* Otherwise print and delete the Error */
 			mpc_err_print(r.error);
 			mpc_err_delete(r.error);
 		}
@@ -62,7 +81,6 @@ int main(int argc, char** argv)
 		free(input);
 	}
 
-	/* Undefine and delete our parsers */
 	mpc_cleanup(4, Number, Operator, Expr, Nlisp);
 	return 0;
 }
